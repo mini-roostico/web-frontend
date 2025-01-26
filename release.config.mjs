758056dea10c75278fbe4593cab2d0b5ac5742b9
FreshMag <pescomagnani@gmail.com>
2025-01-26 18:41:28 +0100

    build: add release.config.mjs and preconfigured-conventional-commits

---
+++ release.config.mjs
@@ -0,0 +1,7 @@
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
+
+config.plugins.push(
+  "@semantic-release/github",
+  "@semantic-release/git",
+)
+export default config
